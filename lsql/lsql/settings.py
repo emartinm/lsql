90a60ed2e40cc39489887152d753f979c10faa9b
Enrique Martín <emartinm@ucm.es>
2020-07-23 20:32:55 +0200

    Eliminado la aplicación polls/

--- lsql/lsql/settings.py
+++ lsql/lsql/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'judge.apps.JudgeConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
