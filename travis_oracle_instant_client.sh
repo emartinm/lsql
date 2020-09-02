0ccd09f37a68fd3b0546cad5d8bebf8c7a512b39
Enrique Martín <emartinm@ucm.es>
2020-09-02 21:18:12 +0200

    Instant Client for Travis

---
+++ travis_oracle_instant_client.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /tmp
+wget https://download.oracle.com/otn_software/linux/instantclient/19800/instantclient-basic-linux.x64-19.8.0.0.0dbru.zip	
+unzip instantclient-basic-linux.x64-19.8.0.0.0dbru.zip
