66c6d249cd55976607e9422dce30b67ba61d522b
Enrique Martín <emartinm@ucm.es>
2021-04-06 23:31:10 +0200

    Actualizacion a Django 3.2

--- lsql/judge/apps.py
+++ lsql/judge/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 
 
-class JudgeConfig(AppConfig):
+class JudgeConfig(AppConfig):  # pragma: no cover
+    # Avoids coverage checking for the JudgeConfig class
     name = 'judge'
-
+    # Default type for auto fields (primary keys not assigned, as in Problem, Submission, etc.)
+    default_auto_field = 'django.db.models.AutoField'
+    
     def ready(self):
         import judge.signals
-
