c400191d3c5e469aa88cbc5b3aa0957ae638dc85
Enrique Martín <emartinm@ucm.es>
2021-04-07 10:57:38 +0200

    BitAutoField por defecto para claves primarias (por defecto a partir de Django 3.2)
    Eliminadas dependencias que ya no son necesarias de requirements.txt

--- lsql/judge/apps.py
+++ lsql/judge/apps.py
@@ -3,9 +3,12 @@ from django.apps import AppConfig
 
 class JudgeConfig(AppConfig):  # pragma: no cover
     # Avoids coverage checking for the JudgeConfig class
+
     name = 'judge'
+
     # Default type for auto fields (primary keys not assigned, as in Problem, Submission, etc.)
-    default_auto_field = 'django.db.models.AutoField'
-    
+    # BigAutoField is the default auto field from Django 3.2
+    default_auto_field = 'django.db.models.BigAutoField'
+
     def ready(self):
         import judge.signals
