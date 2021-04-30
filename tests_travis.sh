c868c7609e721272c8284ce9312df03a196654c5
Enrique Martín <emartinm@ucm.es>
2021-04-30 15:12:50 +0200

    Comprueba el checksum de codecov antes de subir el informe de tests

--- tests_travis.sh
+++ tests_travis.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Prints environment
-echo "Environment"
-env
-echo "-----------"
+# echo "Environment"
+# env
+# echo "-----------"
 
 # Applies migrations, run tests and uploads reports to codecov
 cd lsql
@@ -16,6 +16,19 @@ coverage erase
 coverage run manage.py test
 coverage report -m
 
-# Generates XML report for codecov,failing if coverage is less than 100%
+# Generates XML report for codecov, failing if coverage is less than 100%
 coverage xml --fail-under 100
-bash <(curl -s https://codecov.io/bash) 
+
+# Submits coverage report, checking the checksum of the uploader
+curl -s https://raw.githubusercontent.com/codecov/codecov-bash/master/SHA1SUM | grep codecov > SHA1SUM
+curl -s https://codecov.io/bash > codecov
+sha1sum -c SHA1SUM
+if [ $? -eq 0 ]; then
+    bash codecov
+else
+    echo "'codecov' uploader does not match expected checksum!"
+    echo "*IGNORING codecov EXECUTION*"
+    echo
+fi
+# Unsafe version that executes without checking
+# bash <(curl -s https://codecov.io/bash)
