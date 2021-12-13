ed012a5a7a07d3493e8dfd6a6a4d47aa0a625fdc
Enrique Martín <emartinm@ucm.es>
2021-12-13 22:28:13 +0100

    Updated requirements.txt and DES

--- travis_des.sh
+++ travis_des.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 cd
-wget https://downloads.sourceforge.net/project/des/devel/2021-10-20/DESDevelLinux64SICStus.zip
+rm -rf ./des/
+rm DESDevelLinux64SICStus.zip
+wget https://downloads.sourceforge.net/project/des/devel/2021-12-12/DESDevelLinux64SICStus.zip
 unzip DESDevelLinux64SICStus.zip
 rm DESDevelLinux64SICStus.zip
 cd des
