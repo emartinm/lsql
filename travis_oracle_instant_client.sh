0940868ed4803ea072fa0e4c2aee980c90777421
Enrique Martín <emartinm@ucm.es>
2022-08-12 17:24:55 +0200

    Testing GitHub actions with oracle

--- travis_oracle_instant_client.sh
+++ travis_oracle_instant_client.sh
@@ -5,4 +5,3 @@ cd /tmp
 wget https://download.oracle.com/otn_software/linux/instantclient/217000/instantclient-basic-linux.x64-21.7.0.0.0dbru.zip
 unzip instantclient-basic-linux.x64-21.7.0.0.0dbru.zip
 ln -s /tmp/instantclient_21_7 /tmp/instantclient
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/instantclient
