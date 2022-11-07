d6f7e5bb038a0e2baea845fc38d347a6ed46a812
Enrique Martín <emartinm@fdi.ucm.es>
2022-11-07 15:49:58 +0100

    Actualizado pylint con arregla para que funcione

--- pylint.sh
+++ pylint.sh
@@ -2,4 +2,9 @@
 
 cd lsql 
 
+# Workaround for django-pylint issue with  pylint >= 2.15
+# https://github.com/PyCQA/pylint-django/issues/370
+# https://github.com/PyCQA/pylint-django/issues/325
+export PYTHONPATH=$PWD
+
 pylint --ignore=apps.py,migrations judge/
