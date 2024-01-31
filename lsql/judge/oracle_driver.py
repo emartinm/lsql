0842d106051adb6589036042be34fcac9927838f
Enrique Martín <emartinm@ucm.es>
2024-01-31 17:54:02 +0100

    Update cx-Oracle to oracledb

--- lsql/judge/oracle_driver.py
+++ lsql/judge/oracle_driver.py
@@ -5,8 +5,8 @@ Copyright Enrique Martín <emartinm@ucm.es> 2020
 Class to connect to Oracle and execute the different types of problems
 """
 
-# Needs to use cx_Oracle
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kike/xDownload/instantclient_19_6
+
+# Requires Oracle Client 19 (LTS) to connect to Oracle Database 11.2 or later in oracledb "thick mode"
 
 import string
 import random
@@ -14,7 +14,7 @@ import os
 import re
 import json
 from typing import Optional
-import cx_Oracle
+import oracledb
 from logzero import logger
 import sqlparse
 
@@ -205,7 +205,7 @@ def get_all_tables(conn):
             # both versions for table names, ignoring possible exceptions.
             try:
                 cursor.execute(f"SELECT * FROM {table_name}")  # Direct name
-            except cx_Oracle.DatabaseError:
+            except oracledb.DatabaseError:
                 cursor.execute(f'SELECT * FROM "{table_name}"')  # Quoted name
             table = table_from_cursor(cursor)
             db_dict[table_name] = table
@@ -280,7 +280,7 @@ def get_compilation_errors(conn):
         return table_from_cursor(cursor)
 
 
-def offset_from_oracle_exception(excp: cx_Oracle.DatabaseError) -> int:
+def offset_from_oracle_exception(excp: oracledb.DatabaseError) -> int:
     """Extracts the offset from a DataBaseError"""
     # pylint: disable = no-member
     # Locally disabled member checks because pylint thinks oracle_error is str
@@ -326,7 +326,7 @@ def is_tle_exception(error_msg):
 
 def build_dsn_tns():
     """Build a Data Source Name from values in the environment"""
-    dsn_tns = cx_Oracle.makedsn(
+    dsn_tns = oracledb.makedsn(
         os.environ['ORACLE_SERVER'],
         int(os.environ['ORACLE_PORT']),
         os.environ['ORACLE_SID'])
@@ -376,16 +376,17 @@ class OracleExecutor:
         possible to create the pool
         """
         self.dsn_tns = build_dsn_tns()
-        self.connection_pool = cx_Oracle.SessionPool(
-            os.environ['ORACLE_USER'],
-            os.environ['ORACLE_PASS'],
-            self.dsn_tns,
-            threaded=True,
-            encoding='UTF-8', nencoding='UTF-8',
-            min=1,
+        oracledb.init_oracle_client()  # To enable "thick mode"
+        self.connection_pool = oracledb.create_pool(
+            user=os.environ['ORACLE_USER'],
+            password=os.environ['ORACLE_PASS'],
+            dsn=self.dsn_tns,
+            # encoding='UTF-8', nencoding='UTF-8',
+            # min=1,
+            min=int(os.environ['ORACLE_MAX_GESTOR_CONNECTIONS']),
             max=int(os.environ['ORACLE_MAX_GESTOR_CONNECTIONS']),
-            getmode=cx_Oracle.SPOOL_ATTRVAL_TIMEDWAIT,
-            waitTimeout=int(os.environ['ORACLE_GESTOR_POOL_TIMEOUT_MS'])
+            getmode=oracledb.POOL_GETMODE_TIMEDWAIT,
+            wait_timeout=int(os.environ['ORACLE_GESTOR_POOL_TIMEOUT_MS'])
         )
         self.version = None
         logger.debug('Created an OracleExecutor to %s with a pool of %s connections with a timeout of %s ms',
@@ -445,7 +446,7 @@ class OracleExecutor:
                     for connection in connections:  # pragma: no cover
                         cursor.execute(self.__KILL_SESSION.format(connection[0], connection[1]))
                     self.drop_user(user[0], gestor)
-        except cx_Oracle.DatabaseError as excp:  # pragma: no cover
+        except oracledb.DatabaseError as excp:  # pragma: no cover
             logger.error('Unable to remove dangling users. Reason: %s', excp)
         finally:
             if gestor is not None:
@@ -465,7 +466,7 @@ class OracleExecutor:
                 cursor.execute(self.__NUM_DANGLING_USERS, age_seconds=age_seconds)
                 row = cursor.fetchone()
                 num = row[0]
-        except cx_Oracle.DatabaseError as excp:  # pragma: no cover
+        except oracledb.DatabaseError as excp:  # pragma: no cover
             logger.error('Unable to get number of dangling users. Reason: %s', excp)
         finally:
             if gestor is not None:
@@ -490,7 +491,7 @@ class OracleExecutor:
         :param passwd: Password of the Oracle user
         :return: Oracle connection
         """
-        connection = cx_Oracle.connect(user, passwd, self.dsn_tns, encoding='UTF-8', nencoding='UTF-8')
+        connection = oracledb.connect(user=user, password=passwd, dsn=self.dsn_tns)
         return connection
 
     def execute_select_test(self, creation, insertion, select, output_db=False):
@@ -541,7 +542,7 @@ class OracleExecutor:
             self.connection_pool.release(gestor)
             gestor = None
             return {"result": result, "db": db}
-        except cx_Oracle.DatabaseError as excp:
+        except oracledb.DatabaseError as excp:
             error_msg = str(excp)
             logger.info('Error when testing SELECT statements: %s - %s - %s', state, excp, select)
             if is_tle_exception(error_msg) and state == OracleStatusCode.EXECUTE_USER_CODE:
@@ -560,7 +561,7 @@ class OracleExecutor:
                     # "is currently connected". This looks like a bug or undocumented behavior of cx_Oracle
                     # These users must be removed manually later
                     self.drop_user(user, gestor)
-                except cx_Oracle.DatabaseError as drop_except:  # pragma: no cover
+                except oracledb.DatabaseError as drop_except:  # pragma: no cover
                     logger.error('Unable to drop user %s, REMOVE IT MANUALLY (%s)', user, drop_except)
             if gestor:
                 self.connection_pool.release(gestor)
@@ -627,7 +628,7 @@ class OracleExecutor:
             gestor = None
 
             return {'pre': pre, 'post': post}
-        except cx_Oracle.DatabaseError as excp:
+        except oracledb.DatabaseError as excp:
             error_msg = str(excp)
             logger.info('Error when testing DML statements: %s - %s - %s', state, excp, stmt)
             if is_tle_exception(error_msg) and state == OracleStatusCode.EXECUTE_USER_CODE:
@@ -643,7 +644,7 @@ class OracleExecutor:
                     # "is currently connected". This looks like a bug or undocumented behavior of cx_Oracle
                     # These users must be removed manually later
                     self.drop_user(user, gestor)
-                except cx_Oracle.DatabaseError as drop_except:  # pragma: no cover
+                except oracledb.DatabaseError as drop_except:  # pragma: no cover
                     logger.error('Unable to drop user %s, REMOVE IT MANUALLY (%s)', user, drop_except)
             if gestor:
                 self.connection_pool.release(gestor)
@@ -719,7 +720,7 @@ class OracleExecutor:
             gestor = None
 
             return {'db': db, 'results': results}
-        except cx_Oracle.DatabaseError as excp:
+        except oracledb.DatabaseError as excp:
             error_msg = str(excp)
             logger.info('Error when testing function statements: %s - %s - %s', state, excp, stmt)
             if is_tle_exception(error_msg) and state == OracleStatusCode.EXECUTE_USER_CODE:
@@ -735,7 +736,7 @@ class OracleExecutor:
                     # "is currently connected". This looks like a bug or undocumented behavior of cx_Oracle
                     # These users must be removed manually later
                     self.drop_user(user, gestor)
-                except cx_Oracle.DatabaseError as drop_except:  # pragma: no cover
+                except oracledb.DatabaseError as drop_except:  # pragma: no cover
                     logger.error('Unable to drop user %s, REMOVE IT MANUALLY (%s)', user, drop_except)
             if gestor:
                 self.connection_pool.release(gestor)
@@ -811,7 +812,7 @@ class OracleExecutor:
             gestor = None
 
             return {'pre': db, 'post': post}
-        except cx_Oracle.DatabaseError as excp:
+        except oracledb.DatabaseError as excp:
             error_msg = str(excp)
             logger.info('Error when testing procedure creation and call: %s - %s - %s', state, excp, stmt)
             if is_tle_exception(error_msg) and state == OracleStatusCode.EXECUTE_USER_CODE:
@@ -827,7 +828,7 @@ class OracleExecutor:
                     # "is currently connected". This looks like a bug or undocumented behavior of cx_Oracle
                     # These users must be removed manually later
                     self.drop_user(user, gestor)
-                except cx_Oracle.DatabaseError as drop_except:  # pragma: no cover
+                except oracledb.DatabaseError as drop_except:  # pragma: no cover
                     logger.error('Unable to drop user %s, REMOVE IT MANUALLY (%s)', user, drop_except)
             if gestor:
                 self.connection_pool.release(gestor)
@@ -897,7 +898,7 @@ class OracleExecutor:
             gestor = None
 
             return {'pre': db, 'post': post}
-        except cx_Oracle.DatabaseError as excp:
+        except oracledb.DatabaseError as excp:
             error_msg = str(excp)
             logger.info('Error when testing trigger creation and call: %s - %s - %s', state, excp, stmt)
             if is_tle_exception(error_msg) and state == OracleStatusCode.EXECUTE_USER_CODE:
@@ -917,7 +918,7 @@ class OracleExecutor:
                     # "is currently connected". This looks like a bug or undocumented behavior of cx_Oracle
                     # These users must be removed manually later
                     self.drop_user(user, gestor)
-                except cx_Oracle.DatabaseError as drop_except:  # pragma: no cover
+                except oracledb.DatabaseError as drop_except:  # pragma: no cover
                     logger.error('Unable to drop user %s, REMOVE IT MANUALLY (%s)', user, drop_except)
             if gestor:
                 self.connection_pool.release(gestor)
@@ -975,7 +976,7 @@ class OracleExecutor:
             self.connection_pool.release(gestor)
             gestor = None
             return {"result_correct": result_correct, "result_incorrect": result_incorrect}
-        except cx_Oracle.DatabaseError as excp:
+        except oracledb.DatabaseError as excp:
             error_msg = str(excp)
             logger.info('Error when testing DISCRIMINANT problem: %s - %s - %s - %s - %s', state, excp, insertion_user,
                         select_correct, select_incorrect)
@@ -997,7 +998,7 @@ class OracleExecutor:
                     # "is currently connected". This looks like a bug or undocumented behavior of cx_Oracle
                     # These users must be removed manually later
                     self.drop_user(user, gestor)
-                except cx_Oracle.DatabaseError as drop_except:  # pragma: no cover
+                except oracledb.DatabaseError as drop_except:  # pragma: no cover
                     logger.error('Unable to drop user %s, REMOVE IT MANUALLY (%s)', user, drop_except)
             if gestor:
                 self.connection_pool.release(gestor)
