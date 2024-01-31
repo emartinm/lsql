2332426a6fd90b0e76152fb766a62a82474dfd27
emartinm <emartinm@ucm.es>
2024-01-31 18:08:27 +0100

    Update cx-Oracle to oracledb (#280)

--- travis_oracle_instant_client.sh
+++ travis_oracle_instant_client.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 cd /tmp
-# wget https://download.oracle.com/otn_software/linux/instantclient/19800/instantclient-basic-linux.x64-19.8.0.0.0dbru.zip
-wget -nv https://download.oracle.com/otn_software/linux/instantclient/217000/instantclient-basic-linux.x64-21.7.0.0.0dbru.zip
-unzip instantclient-basic-linux.x64-21.7.0.0.0dbru.zip
-ln -s /tmp/instantclient_21_7 /tmp/instantclient
+# wget -nv https://download.oracle.com/otn_software/linux/instantclient/217000/instantclient-basic-linux.x64-21.7.0.0.0dbru.zip
+# unzip instantclient-basic-linux.x64-21.7.0.0.0dbru.zip
+# ln -s /tmp/instantclient_21_7 /tmp/instantclient
+wget -nv https://download.oracle.com/otn_software/linux/instantclient/1921000/instantclient-basic-linux.x64-19.21.0.0.0dbru.zip
+unzip instantclient-basic-linux.x64-19.21.0.0.0dbru.zip
+ln -s /tmp/instantclient_19_21 /tmp/instantclient
+
