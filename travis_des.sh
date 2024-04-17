78fdedf643c7ffcfbd449bce249b9a8b58f47b39
Enrique Martín <emartinm@ucm.es>
2024-04-17 18:40:19 +0200

    Arreglando GitHub Action con ruta de DES

--- travis_des.sh
+++ travis_des.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 cd
+echo $PWD
 rm -rf ./des/
 rm DESDevelLinux64SICStus.zip
 wget -nv https://downloads.sourceforge.net/project/des/devel/2021-12-12/DESDevelLinux64SICStus.zip
 unzip DESDevelLinux64SICStus.zip
 rm DESDevelLinux64SICStus.zip
 cd des
+echo $PWD
 chmod u+x des des_start
