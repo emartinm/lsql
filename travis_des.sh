1be1cc073f06c4bafcf29a8d0320e9152383119e
Enrique Martín <emartinm@ucm.es>
2024-04-17 18:54:56 +0200

    Arreglar GitHub Action para DES

--- travis_des.sh
+++ travis_des.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
 cd $GITHUB_WORKSPACE
-echo $PWD
 rm -rf ./des/
 rm DESDevelLinux64SICStus.zip
 wget -nv https://downloads.sourceforge.net/project/des/devel/2021-12-12/DESDevelLinux64SICStus.zip
 unzip DESDevelLinux64SICStus.zip
 rm DESDevelLinux64SICStus.zip
 cd des
-echo $PWD
 chmod u+x des des_start
+
+DES_PATH=`readlink -f des_start`
+echo "Instalado DES en ${DES_PATH}"
