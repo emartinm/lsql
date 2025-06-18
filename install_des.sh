e2d9cad8fd7b472c4ec157b54a9d8ef42e451276
Enrique Martín <emartinm@ucm.es>
2025-06-18 14:26:31 +0200

    Actulización de version DES

--- install_des.sh
+++ install_des.sh
@@ -2,10 +2,10 @@
 
 cd $GITHUB_WORKSPACE
 rm -rf ./des/
-rm DESDevelLinux64SICStus.zip
-wget -nv https://downloads.sourceforge.net/project/des/devel/2021-12-12/DESDevelLinux64SICStus.zip
-unzip DESDevelLinux64SICStus.zip
-rm DESDevelLinux64SICStus.zip
+rm DES6.8Linux64SICStus.zip
+wget -nv https://sourceforge.net/projects/des/files/des/des6.8/DES6.8Linux64SICStus.zip/download -O DES6.8Linux64SICStus.zip
+unzip DES6.8Linux64SICStus.zip
+rm DES6.8Linux64SICStus.zip
 cd des
 chmod u+x des des_start
 
