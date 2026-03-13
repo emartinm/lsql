0d8f66336a78be1270722c9e8ef9b59ab497a676
Enrique Martín <emartinm@ucm.es>
2026-03-13 13:23:40 +0100

    Actualización DES

--- install_des.sh
+++ install_des.sh
@@ -3,9 +3,9 @@
 cd $GITHUB_WORKSPACE
 rm -rf ./des/
 rm DES6.8Linux64SICStus.zip
-wget -nv https://sourceforge.net/projects/des/files/des/des6.8/DES6.8Linux64SICStus.zip/download -O DES6.8Linux64SICStus.zip
-unzip DES6.8Linux64SICStus.zip
-rm DES6.8Linux64SICStus.zip
+wget -nv https://sourceforge.net/projects/des/files/devel/2026-03-10/DESDevelLinux64SICStus.zip/download -O DESLinux64SICStus.zip
+unzip DESLinux64SICStus.zip
+rm DESLinux64SICStus.zip
 cd des
 chmod u+x des des_start
 
